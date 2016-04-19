f4066e2fbeeea6effa90b8aa5c0f64ce9af708a9
Vince Bloise <vbloise3@verizon.net>
2016-04-18 21:12:59 -0400

    Added some insert and query example ruby code

--- connect.rb
+++ connect.rb
@@ -5,3 +5,17 @@ $client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'tutorial')
 Mongo::Logger.logger.level = ::Logger::ERROR
 $users = $client[:users]
 puts 'connected!'
+
+bloise = {"last_name" => "bloise", "age" => 54}
+dude = {"last_name" => "The Dude", "age" => 45}
+
+#bloise_id = $users.insert_one(bloise)
+#dude_id = $users.insert_one(dude)
+
+puts "Valid users:"
+$users.find().each do |user|
+    puts user
+end
+
+puts "Users older than 20"
+p $users.find("age" => {"$gt" => 20}).each.to_a
